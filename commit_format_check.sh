e8ac7376f402917839ca3d0ea2a8c6458f7f79a0
Adrien Ducourthial <65553499+aducourthial@users.noreply.github.com>
2021-01-12 11:43:47 +0100

    Update commit_format_check.sh

--- commit_format_check.sh
+++ commit_format_check.sh
@@ -6,6 +6,6 @@ if [[ "$MESSAGE" =~ ^(chg|fix|new)(: )((dev|usr|pkg|test|doc)(: ))?.*(\!|\@)?.*$
 then
   exit 0
 else
-  echo -e "Error in commit message, pls begin by dev:, chg: or fix:" >&2
+  echo -e "Error in commit message, pls begin by new:, chg: or fix:" >&2
   exit 1
 fi
